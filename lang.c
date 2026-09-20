8adb6255eff3802c07d13494646ae59ea184b43a
mw lee <auddn126@gmail.com>
2026-09-20 17:40:39 +0900

    렉서 2단계
    소스코드를 받아서 토큰화하기

--- lang.c
+++ lang.c
@@ -24,35 +24,171 @@ bool is_korean(unsigned char c) // 한글은 UTF-8로 표현되고 첫바이트�
 
 int main(void)
 {
-    char *source = "연봉 = 500;";
+    char *source = "연봉최고 연봉은 = 500 + 1 + n++;";
     char *idx = source;
-    printf("%s\n", idx);
+    char *start;
+    char buf[999] = {};
+    int len;
+
+    printf("소스코드 : %s\n\n\n\n", source);
 
     while (*idx != '\0')
     {
         if (is_korean(*idx))
         {
-            printf("한글입니다.\n");
-            for (int i = 0; i < 3; i++)
+            start = idx;
+
+            while (is_korean(*idx))
             {
-                idx++;
+                idx += 3; // idx포인터가 지금 서있는곳이 "한글"이라면 3바이트를 건너뛰어야만 다음 char을 point할 수 있다.
             }
+
+            len = idx - start;        // 만약 포인터가 int였고 포인터와 포인터를 뺄셈한다
+                                      // 가정하자 주소값의 차이가 12바이트이면 cal에는 3이 들어간다 int 3개가 들어가는 자리
+            strncpy(buf, start, len); // start가 가르키는 char부터 len만큼 buf에 저장하기.
+            buf[len] = '\0';
+
+            printf("한글토큰 : %s\n", buf);
+
             continue;
         }
 
         else if (isalpha(*idx))
         {
-            printf("알파벳입니다.\n");
+
+            start = idx;
+
+            while (isalpha(*idx))
+            {
+                idx++;
+            }
+
+            len = idx - start;
+
+            strncpy(buf, start, len);
+            buf[len] = '\0';
+
+            printf("알파벳토큰 : %s\n", buf);
         }
         else if (isdigit(*idx))
         {
-            printf("숫자입니다.\n");
+            start = idx;
+            while (isdigit(*idx))
+            {
+                idx++;
+            }
+            len = idx - start;
+            strncpy(buf, start, len);
+            buf[len] = '\0';
+
+            printf("숫자토큰 : %s\n", buf);
         }
         else if (isspace(*idx))
         {
-            printf("공백입니다.\n");
+            start = idx;
+            while (isspace(*idx))
+            {
+                idx++;
+            }
+            len = idx - start;
+            strncpy(buf, start, len);
+            buf[len] = '\0';
+
+            printf("공백토큰 : %s\n", buf);
         }
-        idx++;
+        else if (*idx == '+')
+        {
+            start = idx;
+
+            if (*(idx + 1) == '+') // 단항연산자라면
+            {
+                idx += 2;
+
+                len = idx - start;
+                strncpy(buf, start, len);
+                buf[len] = '\0';
+
+                printf("단항연산토큰 : %s\n", buf);
+            }
+            else // 이항연산이라면
+            {
+                idx++;
+
+                len = idx - start;
+                strncpy(buf, start, len);
+                buf[len] = '\0';
+
+                printf("이항연산토큰 : %s\n", buf);
+            }
+        }
+
+        else if (*idx == '-')
+        {
+            start = idx;
+
+            if (*(idx + 1) == '-') // 단항연산자라면
+            {
+                idx += 2;
+
+                len = idx - start;
+                strncpy(buf, start, len);
+                buf[len] = '\0';
+
+                printf("단항연산토큰 : %s\n", buf);
+            }
+            else // 이항연산이라면
+            {
+                idx++;
+
+                len = idx - start;
+                strncpy(buf, start, len);
+                buf[len] = '\0';
+
+                printf("이항연산토큰 : %s\n", buf);
+            }
+        }
+
+        else if (*idx == '=')
+        {
+            start = idx;
+
+            idx++;
+
+            len = 1;
+
+            strncpy(buf, start, len);
+            buf[len] = '\0';
+
+            printf("대입연산토큰 : %s\n", buf);
+        }
+
+        else if (*idx == ';')
+        {
+            start = idx;
+
+            idx++;
+
+            len = 1;
+
+            strncpy(buf, start, len);
+            buf[len] = '\0';
+
+            printf("세미콜론토큰 : %s\n", buf);
+        }
+
+        else
+        {
+            start = idx;
+
+            idx++;
+
+            len = 1;
+
+            strncpy(buf, start, len);
+            buf[len] = '\0';
+
+            printf("미지원기능토큰 : %s\n", buf);
+        }
     }
 
     return 0;
